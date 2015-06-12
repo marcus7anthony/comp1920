f67d37afbb70af6fbfa5a57031edf10a26e32e10
Marcus Anthony <me@marcusanthony.com>
2015-06-11 19:17:27 -0700

    Create array.php

---
+++ lab7/array.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Author: Marcus Anthony
+File: index.php
+Version: 1.0
+Last modified: May 14, 2015
+*/
+
+/* This file contains an array of elements. */
+
+$array[0] = "Select one...";
+$array[1] = "Traveled to 13 countries so far.";
+$array[2] = "The Pinnacle List is a website I own.";
+$array[3] = "Grew up with my dog, who is 14 years old today.";
+$array[4] = "Broke up my engagement because being single is more productive.";
+$array[5] = "I am a super Canucks fan.";
+$array[6] = "I grew up with what \"Nintendon't\", therefore I was a SEGA fanboy.";
+$array[7] = "Today I play video games on Xbox One.";
+$array[8] = "I am half Polish, half Italian.";
+$array[9] = "California dreamin' everyday.";
+$array[10] = "My favorite music comes from Michael Jackson, John Lennon, George Harrison, and Pharrell Williams.";
+
+?>
