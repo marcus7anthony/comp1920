43694aae13663e45dfbc1126106af11883cfff68
Marcus Anthony <me@marcusanthony.com>
2015-06-11 19:57:17 -0700

    Create get_text.php

---
+++ lab10/get_text.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Author: Marcus Anthony
+File: get_text.php
+Version: 1.0
+Last modified: May 21, 2015
+*/
+?>
+<!doctype html>
+<html>
+<head>
+	<title>COMP 1920 - Lab 10</title>
+</head>
+<body>
+	<form action="post.php" method="post" name="post">
+	<textarea name="textarea" rows="5" cols="30"></textarea>
+	<br><br>
+	<input type="submit" name="submit" value="Submit Query">
+	</form>
+</body>
+</html>
